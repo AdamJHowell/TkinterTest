d43b2609acb620e1905807641f0268de1a18368e
Adam Howell <AdamJHowell@users.noreply.github.com>
2024-04-12 19:59:50 -0600

    Converting to a file search UI.

--- MultiLineLabel.py
+++ MultiLineLabel.py
@@ -1,35 +1,42 @@
 import tkinter
+from tkinter import END
 
 
 def verify():
+  print( "\nResults:" )
   # Print to the console.
-  print( user_input.get() )
+  print( directory_value.get() )
   # Print to the Text() object.
-  text_box.insert( tkinter.END, user_input.get() )
+  text_box.insert( tkinter.END, directory_value.get() )
+  text_box.insert( tkinter.END, "\n" )
+  print( extension_value.get() )
 
 
 root = tkinter.Tk()
-root.title( "Grid Test" )
+root.title( "Find files" )
 root.minsize( 200, 200 )  # width, height
 root.geometry( "408x305+50+50" )
-user_input = tkinter.StringVar( root )
+directory_value = tkinter.StringVar( root )
+extension_value = tkinter.StringVar( root )
 
 # First grid row.
-tkinter.Label( root, text = "First name" ).grid( row = 0 )
-entry_first_name = tkinter.Entry( root, bd = 3, textvariable = user_input )
-entry_first_name.grid( row = 0, column = 1, columnspan = 2 )
+tkinter.Label( root, text = "Directory" ).grid( row = 0 )
+directory_entry = tkinter.Entry( root, bd = 3, textvariable = directory_value )
+directory_entry.grid( row = 0, column = 1, columnspan = 2 )
+directory_entry.insert( END, "C:\\Media\\Music\\Rush\\" )
 
 # Second grid row.
-tkinter.Label( root, text = "Last name" ).grid( row = 1 )
-entry_last_name = tkinter.Entry( root, bd = 3 )
-entry_last_name.grid( row = 1, column = 1, columnspan = 2 )
+tkinter.Label( root, text = "Extensions" ).grid( row = 1 )
+extension_entry = tkinter.Entry( root, bd = 3, textvariable = extension_value )
+extension_entry.grid( row = 1, column = 1, columnspan = 2 )
+extension_entry.insert( END, ".flac" )
 
 # Third grid row.
 text_box = tkinter.Text( root, height = 14, width = 50 )
 text_box.grid( row = 2, columnspan = 3 )
 
 # Last grid row.
-tkinter.Button( root, text = "Verify", command = verify ).grid( row = 6, column = 0 )
+tkinter.Button( root, text = "Search", command = verify ).grid( row = 6, column = 0 )
 tkinter.Button( root, text = "Exit", command = root.quit ).grid( row = 6, column = 1 )
 
 root.mainloop()
