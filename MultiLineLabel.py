06ebfbf11a13374b99cf3461025ef91eac64b789
AdamJHowell <adam.j.howell@gmail.com>
2024-04-12 18:13:39 -0600

    Added more example programs.

--- MultiLineLabel.py
+++ MultiLineLabel.py
@@ -11,22 +11,22 @@ def verify():
 root = tkinter.Tk()
 root.title( "Grid Test" )
 root.minsize( 200, 200 )  # width, height
-root.geometry( "500x300+50+50" )
+root.geometry( "408x305+50+50" )
 user_input = tkinter.StringVar( root )
 
 # First grid row.
 tkinter.Label( root, text = "First name" ).grid( row = 0 )
 entry_first_name = tkinter.Entry( root, bd = 3, textvariable = user_input )
-entry_first_name.grid( row = 0, column = 1 )
+entry_first_name.grid( row = 0, column = 1, columnspan = 2 )
 
 # Second grid row.
 tkinter.Label( root, text = "Last name" ).grid( row = 1 )
 entry_last_name = tkinter.Entry( root, bd = 3 )
-entry_last_name.grid( row = 1, column = 1 )
+entry_last_name.grid( row = 1, column = 1, columnspan = 2 )
 
 # Third grid row.
-text_box = tkinter.Text( root )
-text_box.grid( row = 2, columnspan = 2 )
+text_box = tkinter.Text( root, height = 14, width = 50 )
+text_box.grid( row = 2, columnspan = 3 )
 
 # Last grid row.
 tkinter.Button( root, text = "Verify", command = verify ).grid( row = 6, column = 0 )
