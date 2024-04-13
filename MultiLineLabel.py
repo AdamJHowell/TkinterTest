83b4f1382775ca16d632095faed2f3fed443a296
Adam Howell <AdamJHowell@users.noreply.github.com>
2024-04-12 20:51:32 -0600

    Brought over the find_files_by_extension() method from FindFiles.

--- MultiLineLabel.py
+++ MultiLineLabel.py
@@ -1,15 +1,44 @@
+import fnmatch
+import os
 import tkinter
 from tkinter import END
+from typing import List
 
 
 def verify():
   print( "\nResults:" )
+  # Retrieve the values.
+  directory = directory_value.get()
+  extensions = [word for word in extension_value.get().split()]
   # Print to the console.
-  print( directory_value.get() )
+  print( directory )
+  print( extensions )
   # Print to the Text() object.
-  text_box.insert( tkinter.END, directory_value.get() )
+  text_box.insert( tkinter.END, directory )
   text_box.insert( tkinter.END, "\n" )
-  print( extension_value.get() )
+  print( "Searching..." )
+  files = find_files_by_extension( directory, extensions )
+  print( "Search complete." )
+  for file in files:
+    text_box.insert( tkinter.END, file )
+    text_box.insert( tkinter.END, "\n" )
+
+
+def find_files_by_extension( directory: str, extensions: List[str] ) -> List[str]:
+  """
+  List all files in the specified directory and its subdirectories that have the given extensions.
+  @param directory: The directory to search for audio files.
+  @param extensions: The extensions to search for.
+  @return: A list of audio files.
+  """
+  print( f"Searching {directory} for extensions {extensions}..." )
+  audio_files = []
+  for root_dir, dirs, files in os.walk( directory ):
+    for extension in extensions:
+      for filename in fnmatch.filter( files, f'*.{extension}' ):
+        audio_files.append( os.path.join( root_dir, filename ) )
+        print( filename )
+  return audio_files
 
 
 root = tkinter.Tk()
@@ -29,7 +58,7 @@ directory_entry.insert( END, "C:\\Media\\Music\\Rush\\" )
 tkinter.Label( root, text = "Extensions" ).grid( row = 1 )
 extension_entry = tkinter.Entry( root, bd = 3, textvariable = extension_value )
 extension_entry.grid( row = 1, column = 1, columnspan = 2 )
-extension_entry.insert( END, ".flac" )
+extension_entry.insert( END, ".flac .m4a" )
 
 # Third grid row.
 text_box = tkinter.Text( root, height = 14, width = 50 )
